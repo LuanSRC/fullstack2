7184f2c3e19a7f799f906af883e33cd7089c54e0
Luan Custódio <luancustodio14@gmail.com>
2025-09-07 20:35:05 -0300

    Created server.js

--- cars-commerce/server.js
+++ cars-commerce/server.js
@@ -1,4 +1,14 @@
+const express = require('express')
 const db = require('./db/db')
 const Car = require('./models/car')
+const PORT = 3000
+
+const app = express()
+
+app.use(express.json())
 
 db.sync()
+
+app.listen(PORT, () => {
+    console.log(`Server running at localhost:${PORT}.`)
+})
